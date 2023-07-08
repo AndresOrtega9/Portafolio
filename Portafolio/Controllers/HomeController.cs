4992916ed8fbb89b39ec08b9718297709cc145f0
naortega <andresortega0218@gmail.com>
2023-07-07 22:37:54 -0500

    Primer commit

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Services;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepositorioProyectos _repositorioProyectos;
+
+        public HomeController(IRepositorioProyectos repositorioProyectos)
+        {
+            _repositorioProyectos = repositorioProyectos;
+        }
+        public IActionResult Index()
+        {
+            var listado = _repositorioProyectos.ObtenerListaDeProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel { Proyectos = listado };
+            return View(modelo);
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        public IActionResult Proyectos()
+        {
+            var proyectos = _repositorioProyectos.ObtenerListaDeProyectos().ToList();
+            return View(proyectos);
+        }
+
+    }
+}
---
+++ Portafolio/Services/RepositorioProyectos.cs
@@ -0,0 +1,35 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Services
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerListaDeProyectos();
+    }
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerListaDeProyectos()
+        {
+            return new List<Proyecto> {
+                new Proyecto{
+                Titulo= "Sistema de matricula",
+                Descripcion="Sistema en el cual...",
+                Link="https://amazon.com",
+                UrlImagen="/imagenes/tesis.png",
+                },
+                new Proyecto{
+                Titulo= "Generador de tickets",
+                Descripcion="Sistema en el cual...",
+                Link="https://google.com",
+                UrlImagen="/imagenes/boleto.png",
+                }, new Proyecto
+                {
+                Titulo= "ReportesUdec",
+                Descripcion="Sistema en el cual..",
+                Link="https://reddit.com",
+                UrlImagen="imagenes/informe.png",
+                }
+            };
+        }
+    }
+}
